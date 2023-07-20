f36ab695fd0816d38288e74edf9536622b669a35
Aditya Kamble <adityakamble6101@gmail.com>
2023-07-20 10:40:05 +0530

    change text.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col min-h-screen pt-10 text-center">
       <h3 className="text-xl font-semibold max-w-[40ch] mx-auto">
-        Posts app created using Vercel postgres database & Server Actions 🚀
+        Posts app created using Vercel's Postgres Database & Server Actions 🚀
       </h3>
 
       <Link href="/posts" className="underline">
